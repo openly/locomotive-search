b2c6b89a262112b55db8f457831cdc82ac729da8
Chetan <chetan@chetan-Inspiron-N5110.(none)>
2014-03-06 10:41:33 +0530

    get CX and key from configuration

--- lib/cc_search.rb
+++ lib/cc_search.rb
@@ -8,7 +8,9 @@ module Locomotive
                 def search
                     if(@data == nil)
                         print "Contacting google CSE for results.\n";
-                        url = "https://www.googleapis.com/customsearch/v1?cx=009946976809586658172%3Avdj_rjjftbq&key=AIzaSyARds6-ZkI0XkRu-6a0H_HcGhtkgOFzgtQ&q=" + @context.registers[:controller].params[:q];
+                        cx = GOOGLE_SEARCH_CX || '009946976809586658172%3Avdj_rjjftbq'
+                        key = GOOGLE_SEARCH_KEY || 'AIzaSyARds6-ZkI0XkRu-6a0H_HcGhtkgOFzgtQ'
+                        url = "https://www.googleapis.com/customsearch/v1?cx=#{cx}&key=#{key}&q=" + @context.registers[:controller].params[:q];
                         @data = JSON.parse(RestClient.get(url));
                     end
                     @data
