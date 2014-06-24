2e34adba723f6620d709da55bcf9de38432e34ea
Chetan <chetan@chetan-Inspiron-N5110.(none)>
2014-06-24 11:35:54 +0530

    query param with space issue fixed

--- lib/cc_search.rb
+++ lib/cc_search.rb
@@ -10,7 +10,9 @@ module Locomotive
                         print "Contacting google CSE for results.\n";
                         cx = ::GOOGLE_SEARCH_CX || '009946976809586658172%3Avdj_rjjftbq'
                         key = ::GOOGLE_SEARCH_KEY || 'AIzaSyARds6-ZkI0XkRu-6a0H_HcGhtkgOFzgtQ'
-                        url = "https://www.googleapis.com/customsearch/v1?cx=#{cx}&key=#{key}&q=" + @context.registers[:controller].params[:q];
+                        queryParam = @context.registers[:controller].params[:q]
+                        queryParam = URI::encode(queryParam)
+                        url = "https://www.googleapis.com/customsearch/v1?cx=#{cx}&key=#{key}&q=" + queryParam;
                         @data = JSON.parse(RestClient.get(url));
                     end
                     @data
